4e3e3ffff9b80087d538d34b38bb776b382b0d86
Paweł Lendzion <pawellendzion01@gmail.com>
2026-08-20 21:50:08 +0200

    feat: add eval of BOOLEAN and NULL comparision

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -74,6 +74,14 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+
+	// The reference comparison works for boolean values
+	// because there are only one TRUE and FALSE, thus every
+	// produced object of boolean shares the same address of one of those
+	case operator == "==":
+		return nativeBoolToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBoolToBooleanObject(left != right)
 	}
 
 	return NULL
