6f653d4db117a8a33a7f8495efcef9ad9ba28074
Paweł Lendzion <pawellendzion01@gmail.com>
2026-08-20 21:20:21 +0200

    feat: add eval to boolean integer infix expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -92,6 +92,14 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return &object.Integer{Value: leftVal * rightVal}
 	case "/":
 		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBoolToBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBooleanObject(leftVal != rightVal)
 	}
 
 	return NULL
