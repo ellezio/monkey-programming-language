50a94283ddd0498834538d4abf6a00a1a3b11108
Paweł Lendzion <pawellendzion01@gmail.com>
2026-08-28 14:42:27 +0200

    feat: builtin len for array

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -3,7 +3,7 @@ package evaluator
 import "monkey/object"
 
 var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{
+	"len": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
@@ -12,6 +12,8 @@ var builtins = map[string]*object.Builtin{
 			switch arg := args[0].(type) {
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -289,6 +289,11 @@ func TestBuiltinFunctions(t *testing.T) {
 		{`len("hello world")`, 11},
 		{`len(1)`, "argument to `len` not supported, got INTEGER"},
 		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
+		{`len([])`, 0},
+		{`len([1, 2, 3])`, 3},
+		{`let a = [1, 2, 3]; len(a)`, 3},
+		{`len([1, 2 * 2, 3 + 3])`, 3},
+		{`let a = [1, 2 * 2, 3 + 3]; len(a)`, 3},
 	}
 
 	for _, tt := range tests {
