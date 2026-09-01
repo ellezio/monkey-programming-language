bf765f75d4d27c5ea12fb16146023aab72316598
Paweł Lendzion <pawellendzion01@gmail.com>
2026-09-01 20:31:42 +0200

    feat: add builtin function `puts`

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "monkey/object"
+import (
+	"fmt"
+	"monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -99,6 +102,16 @@ var builtins = map[string]*object.Builtin{
 			elems[length] = args[1]
 
 			return &object.Array{Elements: elems}
+		},
+	},
+
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
